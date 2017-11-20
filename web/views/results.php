d489fff0c09a510d9c3414a397b381defd41b69c
Josh Brown <jobr3255@colorado.edu>
2017-11-20 16:28:30 -0700

    testing require routes

--- web/views/results.php
+++ web/views/results.php
@@ -6,9 +6,21 @@
 <?php 
 require "nav.php";
 require "../handlers/search.php";
+require "../user.php";
+require "../handlers/errorHandler.php";
+$connection = mysqli_connect(HOST, USER,PASS, DB);
+	if(mysqli_connect_errno()){
+		return False;
+	}
 ?>
 <?php
-
+if(isset($_GET['search'])) {
+	$term = $_REQUEST['search'];
+	echo "\n";
+	?>
+	<p id="results">Showing results for: <?php echo $term; ?> </p> <?php echo "\n"; ?>
+	<div id="results"></div> <?php echo "\n";
+}
 ?>
 </body>
 </html>
