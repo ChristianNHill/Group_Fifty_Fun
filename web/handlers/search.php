3ae8f50fe6fc7415ebec2b8266f5042c3b34dc7f
Josh Brown <jobr3255@colorado.edu>
2017-11-26 21:12:44 -0700

    added class search functionality

--- web/handlers/search.php
+++ web/handlers/search.php
@@ -5,15 +5,26 @@ if(isset($_GET['search'])) {
 	$term = $_REQUEST['search'];
 
 	$connection = mysqli_connect(HOST, USER,PASS, DB);
-	
+
 	$query = "select name from school where name rlike '".$term."';";
 	$result = mysqli_query($connection, $query);
+	$no_results = true;
 	if($result){
+		$no_results = false;
 		while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
 			echo "<p>".$row[0]."</p>";
 		}
 	}
-	else{
+	$query = "select * from class where name rlike '".$term."' or department_code rlike '".$term."' or class_code like '".$term."';";
+	$result = mysqli_query($connection, $query);
+	$no_results = true;
+	if($result){
+		$no_results = false;
+		while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+			echo "<p>".$row[2]." ".$row[3]."</br>".$row[4]."</p>";
+		}
+	}
+	if($no_results){
 		echo "No results found";
 	}
 }
